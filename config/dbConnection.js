e8f7a0e6a7e24986714c04b343dc9f5b61b09424
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2021-04-05 14:31:54 -0400

    modified jawsdb connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -12,16 +12,7 @@ const mysql = require('mysql');
 //     multipleStatements: true
 // });
 
-// const connection = mysql.createPool({
-//     host: 'us-cdbr-east-03.cleardb.com',
-//     user: 'b728b897ed6f3f',
-//     password: '6233a1fb',
-//     database: 'heroku_86cf96b9bd6e888',
-//     multipleStatements: true
-// });
-
-
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
     host: 'vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'lb3b9c0qtk3irjkv',
     password: 'zvixroqusrz5ofh1',
@@ -29,12 +20,12 @@ const connection = mysql.createPool({
     multipleStatements: true
 });
 
-// connection.connect(function (err) {
-//     if (err) {
-//         console.error('error connecting: ' + err.stack);
-//         return;
-//     }
-//     console.log('MYSQL CONNECTION OPEN');
-// });
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('MYSQL CONNECTION OPEN');
+});
 
 module.exports = connection;
